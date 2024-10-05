e19d11957adba3d0dd3c44125dfd2aa85619a63f
Тимур Каримов <syryus2006@gmail.com>
2024-10-05 09:01:23 +0300

    Другой код

--- hello.pas
+++ hello.pas
@@ -1,3 +1,12 @@
 ﻿begin
-  print('Hello world')
+  var k:=readinteger('Введите время');
+  var time:string;
+  assert(k in 0..23);
+  case k of
+    4..10:time:='Доброе утро, мир';
+    11..16:time:='Добрый день, мир';
+    17..22:time:='Добрый вечер, мир!';
+    else time:='Доброй ночи, мир!';
+    end;
+  print(time)
 end.
