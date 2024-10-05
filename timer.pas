a2fdead3b9172400bb5baddfc126b9338f5dc9bd
Тимур Каримов <syryus2006@gmail.com>
2024-10-05 09:19:48 +0300

    VREMYA

---
+++ timer.pas
@@ -0,0 +1,7 @@
+﻿begin
+  var a:=readinteger('Введите количество часов');
+  var b:=readinteger('Введите количество минут');
+  var c:=readinteger('Введите количество секунд');
+  print('Всего секунд',a*3600+b*60+c)
+  
+end.
