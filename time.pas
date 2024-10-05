a5bb9fbf6b42821ebc5ebf123c10ae9e7dec3770
Тимур Каримов <syryus2006@gmail.com>
2024-10-05 09:10:31 +0300

    time code

---
+++ time.pas
@@ -0,0 +1,16 @@
+﻿var god:=readinteger;
+begin
+  if god mod 100 =0 then
+  begin
+    print('Не високосный');
+    exit
+  end;
+  if god mod 400 =0 then
+  begin
+    print('Не високосный');
+    exit
+  end;
+  if god mod 4 =0 then print('Високосный')
+  else print('Не високосный')
+    
+end.
