0fb7dfb01a0713bdada0fe60532ee0518f0b426f
Тимур Каримов <syryus2006@gmail.com>
2024-10-05 09:14:41 +0300

    net

--- time.pas
+++ time.pas
@@ -2,15 +2,15 @@
 begin
   if god mod 100 =0 then
   begin
-    print('Не високосный');
+    print('Не високосный,365 дней');
     exit
   end;
   if god mod 400 =0 then
   begin
-    print('Не високосный');
+    print('Не високосный,365 дней');
     exit
   end;
-  if god mod 4 =0 then print('Високосный')
-  else print('Не високосный')
+  if god mod 4 =0 then print('Високосный,366 дней')
+  else print('Не високосный,365 дней')
     
 end.
